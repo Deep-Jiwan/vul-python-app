2fe610cef7690ac0aa4ceb99845d0bdbf513fb96
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-01 21:30:16 +0530

    FINDING 021 - Attempt 1: Fixed Template Injection (CWE-074)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -129,10 +129,14 @@ def profile():
     username = request.args.get('name', session.get('username', 'Guest'))
     
     # --------------------------------------------
+    import html
+    safe_username = html.escape(username, quote=True)
+    safe_comment = html.escape(request.args.get('comment', 'No comment'), quote=True)
+    
     html_content = f'''
         <h1>User Profile</h1>
-        <p>Welcome, {username}!</p>
-        <p>Your comment: {request.args.get('comment', 'No comment')}</p>
+        <p>Welcome, {safe_username}!</p>
+        <p>Your comment: {safe_comment}</p>
     '''
     
     # --------------------------------------------
