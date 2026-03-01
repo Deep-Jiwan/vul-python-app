8a10c9ebe63a65d8e5b56ba2c13284d93e5b903c
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-01 19:14:03 +0530

    FINDING 022 - Attempt 1: Fixed Template Injection (CWE-074)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -151,9 +151,13 @@ def frame_content():
     frame_url = request.args.get('url', 'https://example.com')
     
     # --------------------------------------------
+    # Validate URL scheme to prevent XSS
+    if not frame_url.startswith(('http://', 'https://')):
+        frame_url = 'https://example.com'
+    
     html_content = f'''
         <h2>External Content</h2>
-        <iframe src="{frame_url}" width="800" height="600"></iframe>
+        <iframe src="{html.escape(frame_url, quote=True)}" width="800" height="600"></iframe>
     '''
     return render_template_string(html_content)
 
