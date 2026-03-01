aa4e6e95693035bb593b7aadede025243730c7b0
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-01 12:56:33 +0530

    FINDING 003 - Attempt 2: Fixed Url Redirection (CWE-601)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -166,6 +166,8 @@ def open_redirect():
     target_url = request.args.get('url', '/')
     
     # --------------------------------------------
+    if not target_url.startswith(('/', 'https://example.com')):
+        target_url = '/'
     return redirect(target_url)
 
 
