847a88a9a910ccdb01122226831af14bc739e456
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-01 20:58:52 +0530

    FINDING 015 - Attempt 2: Fixed Weak Cryptographic Algorithm (CWE-327)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -185,15 +185,16 @@ def open_redirect():
 def encrypt_data():
     # --------------------------------------------
     data = request.args.get('data', 'secret message')
-    
+
     # --------------------------------------------
-    key = b'8bytekey'
-    cipher = DES.new(key, DES.MODE_ECB)
-    
+    key = b'16bytekey!!!!'
+    iv = b'16byteiv!!!!!!!!'
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
     # --------------------------------------------
-    padded_data = data + ' ' * (8 - len(data) % 8)
+    padded_data = data + ' ' * (16 - len(data) % 16)
     encrypted = cipher.encrypt(padded_data.encode())
-    
+
     return f"<h2>Encrypted Data:</h2><p>{encrypted.hex()}</p>"
 
 
