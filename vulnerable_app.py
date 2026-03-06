17ce535b919a71875efcd6970707af29ead9bd61
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-06 22:54:01 +0530

    FINDING 025 - Attempt 1: Fixed Reflective Xss (CWE-079)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -166,9 +166,10 @@ def frame_content():
         frame_url = 'https://example.com'
     
     # --------------------------------------------
+    from html import escape
     html_content = f'''
         <h2>External Content</h2>
-        <iframe src="{frame_url}" width="800" height="600"></iframe>
+        <iframe src="{escape(frame_url)}" width="800" height="600"></iframe>
     '''
     return render_template_string(html_content)
 
