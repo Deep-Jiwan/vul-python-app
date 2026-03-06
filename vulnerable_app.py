b326d6cb547c2531d16d11ed92066ab34bcb6156
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-07 03:20:33 +0530

    FINDING 024 - Attempt 1: Fixed Reflective Xss (CWE-079)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -150,7 +150,9 @@ def profile():
 @app.route('/frame_content')
 def frame_content():
     # --------------------------------------------
+    import html
     frame_url = request.args.get('url', 'https://example.com')
+    frame_url = html.escape(frame_url, quote=True)
     
     # --------------------------------------------
     template = '''
