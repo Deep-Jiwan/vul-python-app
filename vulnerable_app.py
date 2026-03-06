7dd6b232d18ee4497858ad1e2a0442dd6add5aa9
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-06 22:27:24 +0530

    FINDING 029 - Attempt 1: Fixed Reflective Xss (CWE-079)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -399,6 +399,7 @@ def ssrf_vulnerability():
 
 @app.route('/account/<account_id>')
 def idor_vulnerability(account_id):
+    import html
     # --------------------------------------------
     conn = sqlite3.connect('vulnerable_app.db')
     cursor = conn.cursor()
@@ -409,7 +410,7 @@ def idor_vulnerability(account_id):
     conn.close()
     
     if account:
-        return f"<h2>Account Details</h2><p>Username: {account[1]}<br>Balance: ${account[2]}<br>Account: {account[3]}</p>"
+        return f"<h2>Account Details</h2><p>Username: {html.escape(str(account[1]))}<br>Balance: ${html.escape(str(account[2]))}<br>Account: {html.escape(str(account[3]))}</p>"
     else:
         return "Account not found"
 
