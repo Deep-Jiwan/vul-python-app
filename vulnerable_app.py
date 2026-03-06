7885490ffb27a954466bffe0b5b5014aaf017744
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-06 22:53:07 +0530

    FINDING 024 - Attempt 1: Fixed Reflective Xss (CWE-079)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -24,6 +24,7 @@ from cryptography.hazmat.primitives.ciphers import modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from markupsafe import escape
+from html import escape
 
 # --------------------------------------------
 ADMIN_USERNAME = "admin"
