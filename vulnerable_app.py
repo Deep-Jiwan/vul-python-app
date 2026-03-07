1205160f5bef0b6642398a216648d8b706027e61
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-07 08:29:19 +0530

    FINDING 007 - Attempt 1: Fixed Reflective Xss (CWE-079)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -385,13 +385,14 @@ def idor_vulnerability(account_id):
 @app.route('/load_object', methods=['POST'])
 def insecure_deserialization():
     import logging
+    import html
     # --------------------------------------------
     data = request.data
     
     try:
         # --------------------------------------------
         obj = pickle.loads(data)
-        return f"<h2>Deserialized Object:</h2><pre>{obj}</pre>"
+        return f"<h2>Deserialized Object:</h2><pre>{html.escape(str(obj), quote=True)}</pre>"
     except Exception as e:
         logging.error(f"Error deserializing: {e}")
         return "An internal error occurred", 500
