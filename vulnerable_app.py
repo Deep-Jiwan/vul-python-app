6a35b3a6097ef1a9b0136017ff63ebd287b2fbec
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-12 12:31:33 +0530

    FINDING 026 - Attempt 1: Fixed Reflective Xss (CWE-079)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -141,7 +141,7 @@ def profile():
     
     # --------------------------------------------
     if username == 'admin':
-        html_content += f'<p style="color:red;">Password Hint: {ADMIN_PASSWORD}</p>'
+        html_content += f'<p style="color:red;">Password Hint: {escape(ADMIN_PASSWORD)}</p>'
     
     return render_template_string(html_content)
 
