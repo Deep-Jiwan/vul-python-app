fa4e27c39793f7374b2fc14b1deada0217145c6e
Deep Mukesh Jiwan <deepjiwan30@gmail.com>
2026-03-12 12:58:09 +0530

    FINDING 029 - Attempt 1: Fixed Sql Injection (CWE-089)

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -99,8 +99,7 @@ def login():
         cursor = conn.cursor()
         
         # --------------------------------------------
-        query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
-        cursor.execute(query)
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
         user = cursor.fetchone()
         conn.close()
         
